6465b471b29a863549cb2c8671b56b74a835cabb
Farruco Sanjurjo <madtrick@gmail.com>
2014-10-08 20:52:49 +0200

    Get the oauth credentials from the ENV

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '147125267354-dbe525glo5mn7jmjnm1cf0hcub4igueh.apps.googleusercontent.com', 'c9a9xzn33Mk_N4mjck1fjof1', {
+  provider :google_oauth2, Figaro.env.google_oauth_cliend_id, Figaro.env.google_oauth_secret, {
     provider_ignores_state: true
   }
 end

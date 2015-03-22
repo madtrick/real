894ea5f87b272f1e9d07c3fad616624a9dcb9218
Farruco Sanjurjo <madtrick@gmail.com>
2015-03-22 13:37:12 +0100

    Create Users Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index]
   resources :accounting_entries, only: [:index, :create, :show, :update]
   resources :recurrent_accounting_entries, only: [:index, :create, :show]
 

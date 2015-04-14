42c0565de03529dca29b1dae53d20e895ee8d571
Farruco Sanjurjo <madtrick@gmail.com>
2015-04-14 20:18:34 +0200

    Add endpoint to update recurrent accounting entries

--- app/controllers/recurrent_accounting_entries_controller.rb
+++ app/controllers/recurrent_accounting_entries_controller.rb
@@ -6,13 +6,29 @@ class RecurrentAccountingEntriesController < ApplicationController
   end
 
   def create
-    @recurrent_accounting_entry = RecurrentAccountingEntry.new(params.permit(:amount, :period, tag_list: []))
+    @recurrent_accounting_entry = RecurrentAccountingEntry.new(recurrent_accounting_entry_params)
     @recurrent_accounting_entry.save!
 
     respond_with @recurrent_accounting_entry
   end
 
   def show
+  end
+
+  def update
+    @recurrent_accounting_entry = RecurrentAccountingEntry.find(params[:id])
+    @recurrent_accounting_entry.update update_recurrent_accounting_entry_params
+
+    respond_with @recurrent_accounting_entry
+  end
+
+  private
+  def recurrent_accounting_entry_params
+    {amount: params[:amount], period: params[:period], tag_list: params[:tags]}
+  end
+
+  def update_recurrent_accounting_entry_params
+    recurrent_accounting_entry_params.merge({last_run: params[:last_run]})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:index]
   resources :accounting_entries, only: [:index, :create, :show, :update]
-  resources :recurrent_accounting_entries, only: [:index, :create, :show]
+  resources :recurrent_accounting_entries, only: [:index, :create, :show, :update]
 
   get '/auth/:provider/callback', to: 'sessions#create'
 end
